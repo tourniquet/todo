b50f421ddf7b09b1b0ad9e8a9a7350d72e604984
tourniquet <admyn3d@gmail.com>
2022-11-15 21:04:54 +0000

    created entry model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+  text = models.TextField()
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    verbose_name_plural = 'entries'
+
+  def __str__(self):
+    return f'{self.text[:50]}...'
