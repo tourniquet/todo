04d949c00ca8d155699a4d9545d445004862483b
tourniquet <admyn3d@gmail.com>
2022-11-16 21:50:08 +0000

    separate page for every entry (which I think is useless)

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'todo'
 urlpatterns = [
   path('', views.index, name='index'),
+  path('entry/<int:entry_id>/', views.entry, name='entry'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Entry
+
 def index(request):
   print(request)
   return render(request, 'todo/index.html')
+
+
+def entry(request, entry_id):
+  entry = Entry.objects.get(id=entry_id)
+  context = { 'entry': entry }
+  print(context)
+  return render(request, 'todo/entry.html', context)
