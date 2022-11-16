6a9eb87e11b61477fefaec388ad351a32086e2e4
tourniquet <admyn3d@gmail.com>
2022-11-16 21:51:09 +0000

    list all entries in a list

--- todo/views.py
+++ todo/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from .models import Entry
 
 def index(request):
-  print(request)
-  return render(request, 'todo/index.html')
+  entries = Entry.objects.order_by('date_added')
+  context = { 'entries': entries }
+  return render(request, 'todo/index.html', context)
 
 
 def entry(request, entry_id):
