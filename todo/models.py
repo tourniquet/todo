062ac25e2669fd54566f2fb379e1269abb38b1d9
tourniquet <admyn3d@gmail.com>
2023-01-13 00:23:14 +0000

    added is_completed column to db model

--- todo/models.py
+++ todo/models.py
@@ -9,6 +9,7 @@ class Entry(models.Model):
   text = models.TextField()
   date_added = models.DateTimeField(auto_now_add=True)
   due_by = models.DateTimeField(default=one_week)
+  is_completed = models.BooleanField(default=False)
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
   class Meta:
